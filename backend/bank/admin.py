f26bf09b268d83a5108fd7548097c007691cd31e
geekwsie-jaime-lopez <geekwsie.jaime.lopez@gmail.com>
2019-12-26 13:24:13 -0800

    modified: fixs to broken backend

--- backend/bank/admin.py
+++ backend/bank/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Bank
+
+class BankAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
 
 # Register your models here.
+admin.site.register(Bank, BankAdmin)
