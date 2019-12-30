d213534746c207b811117775d31729ae7f87d349
geekwsie-jaime-lopez <geekwsie.jaime.lopez@gmail.com>
2019-12-30 14:20:30 -0800

    modified: App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,64 @@
 import React, { Component } from "react";
 import Modal from "./components/Modal";
-import axios from "axios";
+// import axios from "axios";
 
     
-class App extends Component {
+const todoItems = [
+      {
+        id: 1,
+        title: "Go to Market",
+        description: "Buy ingredients to prepare dinner",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Study",
+        description: "Read Algebra and History textbook for upcoming test",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Sally's books",
+        description: "Go to library to rent sally's books",
+        completed: true
+      },
+      {
+        id: 4,
+        title: "Article",
+        description: "Write article on how to use django with react",
+        completed: false
+      }
+    ];
+    class App extends Component {
       constructor(props) {
         super(props);
         this.state = {
+          modal: false,
           viewCompleted: false,
           activeItem: {
             title: "",
             description: "",
             completed: false
           },
-          todoList: []
+          todoList: todoItems
         };
       }
-      componentDidMount() {
-        this.refreshList();
-      }
-      refreshList = () => {
-        axios
-          .get("https://8000-da8401a0-deab-4310-9508-1694f29c8fdb.ws-us02.gitpod.io/api/todos/")
-          .then(res => this.setState({ todoList: res.data }))
-          .catch(err => console.log(err));
+      toggle = () => {
+        this.setState({ modal: !this.state.modal });
+      };
+      handleSubmit = item => {
+        this.toggle();
+        alert("save" + JSON.stringify(item));
+      };
+      handleDelete = item => {
+        alert("delete" + JSON.stringify(item));
+      };
+      createItem = () => {
+        const item = { title: "", description: "", completed: false };
+        this.setState({ activeItem: item, modal: !this.state.modal });
+      };
+      editItem = item => {
+        this.setState({ activeItem: item, modal: !this.state.modal });
       };
       displayCompleted = status => {
         if (status) {
@@ -72,45 +107,17 @@ class App extends Component {
                 onClick={() => this.editItem(item)}
                 className="btn btn-secondary mr-2"
               >
-                {" "}
-                Edit{" "}
+                Edit
               </button>
               <button
                 onClick={() => this.handleDelete(item)}
                 className="btn btn-danger"
               >
-                Delete{" "}
+                Delete
               </button>
             </span>
           </li>
         ));
-      };
-      toggle = () => {
-        this.setState({ modal: !this.state.modal });
-      };
-      handleSubmit = item => {
-        this.toggle();
-        if (item.id) {
-          axios
-            .put(`https://8000-da8401a0-deab-4310-9508-1694f29c8fdb.ws-us02.gitpod.io/api/todos/${item.id}/`, item)
-            .then(res => this.refreshList());
-          return;
-        }
-        axios
-          .post("https://8000-da8401a0-deab-4310-9508-1694f29c8fdb.ws-us02.gitpod.io/api/todos/", item)
-          .then(res => this.refreshList());
-      };
-      handleDelete = item => {
-        axios
-          .delete(`https://8000-da8401a0-deab-4310-9508-1694f29c8fdb.ws-us02.gitpod.io/api/todos/${item.id}`)
-          .then(res => this.refreshList());
-      };
-      createItem = () => {
-        const item = { title: "", description: "", completed: false };
-        this.setState({ activeItem: item, modal: !this.state.modal });
-      };
-      editItem = item => {
-        this.setState({ activeItem: item, modal: !this.state.modal });
       };
       render() {
         return (
