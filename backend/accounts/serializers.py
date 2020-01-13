285b5ceb06b4afa856fe70b45ae11d16c239af3b
geekwsie-jaime-lopez <geekwsie.jaime.lopez@gmail.com>
2020-01-13 13:45:17 -0800

    Modified: accounts/serializers.py added 2 new classes

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,19 +1,36 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from django.contrib.auth import authenticate
 
 # User Serializer
+class PermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        # fields = '__all__'
+        fields = ('name',)
+
 class UserSerializer(serializers.ModelSerializer):
+    groups = PermissionSerializer(many=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
-# Register Serializer
+        fields = ('id', 'username', 'email', 'groups')
+
+    def created(self, validated_data):
+        permissions_data = validated_data.pop('groups')
+        user = User.objects.create(**validated_data)
+        for permission_data in permissions_data:
+            Permission.objects.create(user=user, **permissions_data)
+        return user
+
 class RegisterSerializer(serializers.ModelSerializer):
+    groups = PermissionSerializer(many=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'groups')
         extra_kwargs = {'password': {'write_only': True}}
-    def create(self, validated_data):
+
+    def created(self, validated_data):
         user = User.objects.create_user(
             validated_data['username'],
             validated_data['email'],
@@ -24,6 +41,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+
     def validate(self, data):
         user = authenticate(**data)
         if user and user.is_active:
